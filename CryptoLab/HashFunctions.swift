0113cfe7b18f759e108cfd2da3766ead4e96fdf2
Branko <popovich.branko@gmail.com>
2017-02-04 13:50:00 +0100

    data extension

--- CryptoLab/HashFunctions.swift
+++ CryptoLab/HashFunctions.swift
@@ -63,3 +63,173 @@ public class SHA1Hash: NSObject, HashingFunction {
 		return Data(sha1Data)
 	}
 }
+
+public class SHA224Hash: NSObject, HashingFunction {
+	
+	/**
+	SHA224 hash function for provided data
+	- parameter: Data to hash
+	- returns: SHA224 hash
+	*/
+	public func hash(data dataToHash: Data) -> Data {
+		//prepare structures
+		var sha224Data = [UInt8](repeating: UInt8(), count: Int(SHA224_DIGEST_LENGTH))
+		var data = Data(dataToHash)
+		let dataSize = data.count
+		let dataPointer = UnsafeMutablePointer<UInt8>(mutating: (data as NSData).bytes.bindMemory(to: UInt8.self, capacity: data.count))
+		
+		//calculate sha1
+		SHA224(dataPointer, dataSize, &sha224Data)
+		
+		//destroy structures
+		//		dataPointer.deinitialize()
+		//		dataPointer.deallocate(capacity: dataSize)
+		
+		return Data(sha224Data)
+	}
+}
+
+public class SHA256Hash: NSObject, HashingFunction {
+	
+	/**
+	SHA256 hash function for provided data
+	- parameter: Data to hash
+	- returns: SHA256 hash
+	*/
+	public func hash(data dataToHash: Data) -> Data {
+		//prepare structures
+		var sha256Data = [UInt8](repeating: UInt8(), count: Int(SHA256_DIGEST_LENGTH))
+		var data = Data(dataToHash)
+		let dataSize = data.count
+		let dataPointer = UnsafeMutablePointer<UInt8>(mutating: (data as NSData).bytes.bindMemory(to: UInt8.self, capacity: data.count))
+		
+		//calculate sha1
+		SHA256(dataPointer, dataSize, &sha256Data)
+		
+		//destroy structures
+		//		dataPointer.deinitialize()
+		//		dataPointer.deallocate(capacity: dataSize)
+		
+		return Data(sha256Data)
+	}
+}
+
+public class SHA384Hash: NSObject, HashingFunction {
+	
+	/**
+	SHA384 hash function for provided data
+	- parameter: Data to hash
+	- returns: SHA384 hash
+	*/
+	public func hash(data dataToHash: Data) -> Data {
+		//prepare structures
+		var sha384Data = [UInt8](repeating: UInt8(), count: Int(SHA384_DIGEST_LENGTH))
+		var data = Data(dataToHash)
+		let dataSize = data.count
+		let dataPointer = UnsafeMutablePointer<UInt8>(mutating: (data as NSData).bytes.bindMemory(to: UInt8.self, capacity: data.count))
+		
+		//calculate sha1
+		SHA384(dataPointer, dataSize, &sha384Data)
+		
+		//destroy structures
+		//		dataPointer.deinitialize()
+		//		dataPointer.deallocate(capacity: dataSize)
+		
+		return Data(sha384Data)
+	}
+}
+
+public class SHA512Hash: NSObject, HashingFunction {
+	
+	/**
+	SHA512 hash function for provided data
+	- parameter: Data to hash
+	- returns: SHA512 hash
+	*/
+	public func hash(data dataToHash: Data) -> Data {
+		//prepare structures
+		var sha512Data = [UInt8](repeating: UInt8(), count: Int(SHA512_DIGEST_LENGTH))
+		var data = Data(dataToHash)
+		let dataSize = data.count
+		let dataPointer = UnsafeMutablePointer<UInt8>(mutating: (data as NSData).bytes.bindMemory(to: UInt8.self, capacity: data.count))
+		
+		//calculate sha1
+		SHA512(dataPointer, dataSize, &sha512Data)
+		
+		//destroy structures
+		//		dataPointer.deinitialize()
+		//		dataPointer.deallocate(capacity: dataSize)
+		
+		return Data(sha512Data)
+	}
+}
+
+public extension Data {
+	func md5() -> Data {
+		return MD5Hash().hash(data: self)
+	}
+	
+	func sha1() -> Data {
+		return SHA1Hash().hash(data: self)
+	}
+	
+	func sha224() -> Data {
+		return SHA224Hash().hash(data: self)
+	}
+	
+	func sha256() -> Data {
+		return SHA256Hash().hash(data: self)
+	}
+	
+	func sha384() -> Data {
+		return SHA384Hash().hash(data: self)
+	}
+	
+	func sha512() -> Data {
+		return SHA512Hash().hash(data: self)
+	}
+}
+
+public extension String {
+	
+	func md5() -> Data? {
+		if let selfData = self.data(using: .utf8) {
+			return MD5Hash().hash(data: selfData)
+		}
+		return nil
+	}
+	
+	func sha1() -> Data? {
+		if let selfData = self.data(using: .utf8) {
+			return MD5Hash().hash(data: selfData)
+		}
+		return nil
+	}
+	
+	func sha224() -> Data? {
+		if let selfData = self.data(using: .utf8) {
+			return SHA224Hash().hash(data: selfData)
+		}
+		return nil
+	}
+	
+	func sha256() -> Data? {
+		if let selfData = self.data(using: .utf8) {
+			return SHA256Hash().hash(data: selfData)
+		}
+		return nil
+	}
+	
+	func sha384() -> Data? {
+		if let selfData = self.data(using: .utf8) {
+			return SHA384Hash().hash(data: selfData)
+		}
+		return nil	}
+	
+	func sha512() -> Data? {
+		if let selfData = self.data(using: .utf8) {
+			return SHA512Hash().hash(data: selfData)
+		}
+		return nil
+	}
+}
--- CryptoLabTests/CryptoLabTests.swift
+++ CryptoLabTests/CryptoLabTests.swift
@@ -45,7 +45,7 @@ class CryptoLabTests: XCTestCase {
 	}
 	
 	//MARK: Testing hashing function correctness
-	//Comparation results were obtained from: http://www.sha1-online.com/
+	//Comparation results were obtained from: http://www.sha1-online.com/ if not specified differently
 	
 	func testMD5Correctness() {
 		let correctString = "0fd3dbec9730101bff92acc820befc34"
@@ -68,6 +68,55 @@ class CryptoLabTests: XCTestCase {
 		let hashString = hashFunc.hash(data: testData).hexEncodedString()
 		
 		XCTAssert(hashString == correctString, "SHA1 hash result is not correct")
+		
+	}
+	
+	//Comparation results obtained from: http://www.miniwebtool.com/sha224-hash-generator/
+	func testSHA224Correctness() {
+		let correctString = "425433d1ec90fd9957c43c7b3372c3ad2c08378d3480a962a4244671"
+		
+		let testData = defaultTestString.data(using: .utf8)!
+		let hashFunc = SHA224Hash()
+		
+		let hashString = hashFunc.hash(data: testData).hexEncodedString()
+		
+		XCTAssert(hashString == correctString, "SHA224 hash result is not correct")
+		
+	}
+	
+	func testSHA256Correctness() {
+		let correctString = "a3e49d843df13c2e2a7786f6ecd7e0d184f45d718d1ac1a8a63e570466e489dd"
+		
+		let testData = defaultTestString.data(using: .utf8)!
+		let hashFunc = SHA256Hash()
+		
+		let hashString = hashFunc.hash(data: testData).hexEncodedString()
+		
+		XCTAssert(hashString == correctString, "SHA224 hash result is not correct")
+		
+	}
+	
+	func testSHA384Correctness() {
+		let correctString = "83ca14ebf3005a10f50839742bda82aa607d972a03b1e6a3086e29195ceaf05f038fecdff02aff6e9dcdd273268875f7"
+		
+		let testData = defaultTestString.data(using: .utf8)!
+		let hashFunc = SHA384Hash()
+		
+		let hashString = hashFunc.hash(data: testData).hexEncodedString()
+		
+		XCTAssert(hashString == correctString, "SHA224 hash result is not correct")
+		
+	}
+	
+	func testSHA512Correctness() {
+		let correctString = "811aa0c53c0039b6ead0ca878b096eed1d39ed873fd2d2d270abfb9ca620d3ed561c565d6dbd1114c323d38e3f59c00df475451fc9b30074f2abda3529df2fa7"
+		
+		let testData = defaultTestString.data(using: .utf8)!
+		let hashFunc = SHA512Hash()
+		
+		let hashString = hashFunc.hash(data: testData).hexEncodedString()
+		
+		XCTAssert(hashString == correctString, "SHA224 hash result is not correct")
 		
 	}
 }
