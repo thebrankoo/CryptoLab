187f88c110685630240707f8061c465706ea88ad
Branko <popovich.branko@gmail.com>
2017-03-01 10:30:56 +0100

    hmac preparation

--- CryptoLab/Cipher/Blowfish.swift
+++ CryptoLab/Cipher/Blowfish.swift
@@ -39,4 +39,57 @@ public class BlowfishCoreCipher: NSObject {
 		
 		return Data(outArray)
 	}
+	
+	public func cbcEncrypt(data toEncrypt: Data, withIV initv: Data) -> Data {
+		var outArray = Data.makeUInt8EmptyArray(ofSize: 118)
+		let inArray = toEncrypt.makeUInt8DataPointer()
+		let iv = initv.makeUInt8DataPointer()
+		
+		BF_cbc_encrypt(inArray, &outArray, initv.count, blowfishKey, iv, BF_ENCRYPT)
+		
+		return Data(outArray)
+	}
+	
+	public func cbcDecrypt(data toEncrypt: Data, withIV initv: Data) -> Data {
+		var outArray = Data.makeUInt8EmptyArray(ofSize: 118)
+		let inArray = toEncrypt.makeUInt8DataPointer()
+		let iv = initv.makeUInt8DataPointer()
+		
+		BF_cbc_encrypt(inArray, &outArray, initv.count, blowfishKey, iv, BF_DECRYPT)
+		
+		return Data(outArray)
+	}
+	
+	public func cfb64Encrypt(data toEncrypt: Data, withIV initv: Data) -> Data {
+		var outArray = Data.makeUInt8EmptyArray(ofSize: 118)
+		let inArray = toEncrypt.makeUInt8DataPointer()
+		let iv = initv.makeUInt8DataPointer()
+		var num: Int32 = 0
+		
+		BF_cfb64_encrypt(inArray, &outArray, initv.count, blowfishKey, iv, &num, BF_ENCRYPT)
+		
+		return Data(outArray)
+	}
+	
+	public func cfb64Decrypt(data toEncrypt: Data, withIV initv: Data) -> Data {
+		var outArray = Data.makeUInt8EmptyArray(ofSize: 118)
+		let inArray = toEncrypt.makeUInt8DataPointer()
+		let iv = initv.makeUInt8DataPointer()
+		var num: Int32 = 0
+		
+		BF_cfb64_encrypt(inArray, &outArray, initv.count, blowfishKey, iv, &num, BF_DECRYPT)
+		
+		return Data(outArray)
+	}
+	
+	public func ofb64Encrypt(data toEncrypt: Data, withIV initv: Data) -> Data {
+		var outArray = Data.makeUInt8EmptyArray(ofSize: 118)
+		let inArray = toEncrypt.makeUInt8DataPointer()
+		let iv = initv.makeUInt8DataPointer()
+		var num: Int32 = 0
+		
+		BF_ofb64_encrypt(inArray, &outArray, initv.count, blowfishKey, iv, &num)
+		
+		return Data(outArray)
+	}
 }
---
+++ CryptoLab/Message Auth/HMAC.swift
@@ -0,0 +1,32 @@
+//
+//  HMAC.swift
+//  CryptoLab
+//
+//  Created by Branko Popovic on 3/1/17.
+//  Copyright © 2017 Branko Popovic. All rights reserved.
+//
+
+import Foundation
+
+/*
+OPENSSL_EXPORT const EVP_MD *EVP_md4(void);
+OPENSSL_EXPORT const EVP_MD *EVP_md5(void);
+OPENSSL_EXPORT const EVP_MD *EVP_sha1(void);
+OPENSSL_EXPORT const EVP_MD *EVP_sha224(void);
+OPENSSL_EXPORT const EVP_MD *EVP_sha256(void);
+OPENSSL_EXPORT const EVP_MD *EVP_sha384(void);
+OPENSSL_EXPORT const EVP_MD *EVP_sha512(void);
+/* EVP_md5_sha1 is a TLS-specific |EVP_MD| which computes the concatenation of
+* MD5 and SHA-1, as used in TLS 1.1 and below. */
+OPENSSL_EXPORT const EVP_MD *EVP_md5_sha1(void);
+*/
+
+
+
+public class HMACAuth: NSObject {
+	
+}
+
+class HMACCoreAuth: NSObject {
+	
+}
